4ca3ff09c489fca44efe52ff9acbe0de2cbce3d5
rklpoi5678 <rklpoi5678@naver.com>
2025-12-14 00:53:21 +0900

    Feat: trade서비스 로직 추가

---
+++ src/services/tradeService.js
@@ -0,0 +1,69 @@
+import { BadRequestException } from '../common/exceptions/badRequestException.js';
+import { ForbiddenException } from '../common/exceptions/forbiddenException.js';
+import { NotFoundException } from '../common/exceptions/notFoundException.js';
+import { prisma } from '../configs/prismaClient.js';
+import { CardStatus, TradeItemType } from '../generated/enums.ts';
+import saleRepository from '../repositories/saleRepository.js';
+import tradeRepository from '../repositories/tradeRepository.js';
+import userCardRepository from '../repositories/userCardRepository.js';
+
+/**
+ *  구매자의 교환 요청
+ * @param {string} applicantId -  교환을 신청하는 사용자 ID (로그인된 사용자)
+ * @param {object} tradeData - saleId, offeredUserCardId, description 등
+ */
+async function requestTradeCard({ applicantId, saleId, offeredUserCardId, description }) {
+  if (!saleId || !offeredUserCardId) {
+    throw new NotFoundException('필수 교환 정보(판매 ID, 제시 카드 ID)가 누락되었습니다.');
+  }
+
+  const offeredCard = await userCardRepository.findUserCardDetail(applicantId, offeredUserCardId);
+  if (!offeredCard || offeredCard.userId !== applicantId) {
+    throw new ForbiddenException('제시된 카드에 대한 소유권이 없습니다.');
+  }
+
+  if (offeredCard.status !== CardStatus.OWNED) {
+    throw new BadRequestException('제시된 카드는 현재 판매 또는 교환 중이 아닙니다.');
+  }
+
+  // 교환 대상 Sale  정보 확인
+  const targetSale = await saleRepository.findActiveSaleBySaleId(saleId);
+  console.log(targetSale);
+  if (!targetSale || targetSale.status !== 'ON_SALE') {
+    throw new NotFoundException('교환 대상 카드가 판매 중이 아니거나 존재하지 않습니다.');
+  }
+  if (targetSale.sellerId === applicantId) {
+    throw new BadRequestException('자신의 판매 카드에 교환을 요청할 수 없습니다.');
+  }
+
+  return prisma.$transaction(async (tx) => {
+    const newTrade = await tradeRepository.requestTrade(
+      {
+        applicantId: applicantId,
+        ownerId: targetSale.sellerId,
+        saleId: saleId,
+        description: description,
+        userCardId: targetSale.userCardId,
+      },
+      tx,
+    );
+
+    const tradeId = newTrade.id;
+
+    // history에 기록 (신청자가 게시하는 카드)
+    await tradeRepository.addTradeItem(tradeId, offeredUserCardId, TradeItemType.OFFERED, tx);
+    // history에 기록 (신청자가  원하는 카드)
+    await tradeRepository.addTradeItem(tradeId, targetSale.userCardId, TradeItemType.TARGET, tx);
+
+    await userCardRepository.updateCardStatus(offeredUserCardId, CardStatus.TRADING, tx);
+
+    // notification 새 교환 요청이 도착하였다. 여기쯤
+    return newTrade;
+  });
+}
+
+const tradeService = {
+  requestTradeCard,
+};
+
+export default tradeService;
