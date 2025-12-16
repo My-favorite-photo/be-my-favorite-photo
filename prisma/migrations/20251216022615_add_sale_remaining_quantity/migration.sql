79c02cd75a3f7c1b3532d66f03f1afb777329bfe
rklpoi5678 <rklpoi5678@naver.com>
2025-12-16 22:31:00 +0900

    Feat: 배포를 위한 사전준비

--- prisma/migrations/20251216022615_add_sale_remaining_quantity/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `remainingQuantity` to the `Sale` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Sale" ADD COLUMN     "remainingQuantity" INTEGER NOT NULL;
